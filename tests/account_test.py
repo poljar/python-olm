8f9a44a209742e74da2eac3ce3a59095f6ab5421
poljar (Damir Jelić) <poljar@termina.org.uk>
2018-03-03 13:55:41 +0100

    tests: Add signature verification tests.

--- tests/account_test.py
+++ tests/account_test.py
@@ -1,8 +1,11 @@
 from builtins import int
 
 import pytest
+from hypothesis import given
+from hypothesis.strategies import text
 
 from olm.account import Account, OlmAccountError
+from olm.utility import OlmVerifyError, Utility
 
 
 class TestClass(object):
@@ -49,3 +52,29 @@ class TestClass(object):
 
         assert not alice._account
         assert not alice._buf
+
+    @given(text())
+    def test_valid_signature(self, message):
+        alice = Account()
+
+        signature = alice.sign(message)
+        signing_key = alice.identity_keys()["ed25519"]
+
+        assert signature
+        assert signing_key
+
+        Utility.ed25519_verify(signing_key, message, signature)
+
+    @given(text())
+    def test_invalid_signature(self, message):
+        alice = Account()
+        bob = Account()
+
+        signature = alice.sign(message)
+        signing_key = bob.identity_keys()["ed25519"]
+
+        assert signature
+        assert signing_key
+
+        with pytest.raises(OlmVerifyError):
+            Utility.ed25519_verify(signing_key, message, signature)
