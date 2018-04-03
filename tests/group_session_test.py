9332bd1a8bcc33f6485b641696c250a8112eb7e6
poljar (Damir Jelić) <poljar@termina.org.uk>
2018-04-03 21:54:39 +0200

    tests: Add group session tests.

---
+++ tests/group_session_test.py
@@ -0,0 +1,34 @@
+from builtins import str
+
+from olm.group_session import InboundGroupSession, OutboundGroupSession
+
+
+class TestClass(object):
+    def test_session_create(self):
+        OutboundGroupSession()
+
+    def test_session_id(self):
+        session = OutboundGroupSession()
+        assert isinstance(session.id, str)
+
+    def test_outbound_pickle(self):
+        session = OutboundGroupSession()
+        pickle = session.pickle()
+
+        assert (session.id == OutboundGroupSession.from_pickle(
+            pickle).id)
+
+    def test_inbound_create(self):
+        outbound = OutboundGroupSession()
+        InboundGroupSession(outbound.session_key)
+
+    def test_inbound_pickle(self):
+        outbound = OutboundGroupSession()
+        inbound = InboundGroupSession(outbound.session_key)
+        pickle = inbound.pickle()
+        InboundGroupSession.from_pickle(pickle)
+
+    def test_encrypt(self):
+        outbound = OutboundGroupSession()
+        inbound = InboundGroupSession(outbound.session_key)
+        assert "Test" == inbound.decrypt(outbound.encrypt("Test"))
