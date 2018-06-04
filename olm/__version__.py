695a552b63d2f95942e98bc50da13a039bb71cc9
poljar (Damir Jelić) <poljar@termina.org.uk>
2018-06-04 16:14:16 +0200

    olm: Add __version__.py.

---
+++ olm/__version__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__title__ = "python-olm"
+__description__ = ("python CFFI bindings for the olm "
+                   "cryptographic ratchet library")
+__url__ = "https://github.com/poljar/python-olm"
+__version__ = "0.1"
+__author__ = "Damir Jelić"
+__author_email__ = "poljar@termina.org.uk"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2018 Damir Jelić"
--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import os
+from codecs import open
 from setuptools import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, "olm", "__version__.py"), "r", "utf-8") as f:
+    exec(f.read(), about)
+
 setup(
-    name="python-olm",
-    version="2.2",
-    url="https://git.matrix.org/git/olm/",
-    author='Damir Jelić',
-    author_email="poljar@termina.org.uk",
-    description=("python CFFI bindings for the olm "
-                 "cryptographic ratchet library"),
-    license="Apache License 2.0",
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
+    license=about["__license__"],
     packages=["olm"],
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["olm_build.py:ffibuilder"],
